a70b2e45751f2f81079c96da40f172bb9a49b78f
jwmoonP <jaewonmoon.keti@gmail.com>
2022-11-29 16:36:20 +0900

    update clustering code

--- clust/ML/clustering/interface.py
+++ clust/ML/clustering/interface.py
@@ -1,5 +1,6 @@
 
 from sklearn.cluster import DBSCAN
+import pandas as pd
 def ClusteringByMinPoints(data, minPts=3, method = "DBSCAN"):
     """ 
     
@@ -29,13 +30,38 @@ def ClusteringByMinPoints(data, minPts=3, method = "DBSCAN"):
         result=model.fit_predict(data)
     return result
 
-def clusteringByMethod(feature_dataset, feature_datasetName, model):
+def clusteringByMethod(feature_dataset: [pd.DataFrame], feature_datasetName:[str], model:int, x:int=None, y:int=None):
+    
+    """ make clustering result of multiple dataset bu clustering model name
+
+    Retrieves rows pertaining to the given keys from the Table instance
+    represented by table_handle.  String keys will be UTF-8 encoded.
+
+    Args:
+        feature_dataset: list of multiple dataframe inputs to be clustered
+        feature_datasetName: list of multiple data frame name
+        model: clust model name to be applied modelList = ['som']
+        x: x length
+        y: y length
+
+    Returns:
+        A dict mapping keys to the corresponding clustering result number. 
+        example:
+        {b'ICW0W2000011': '5',
+         b'ICW0W2000013': '4',
+         b'ICW0W2000014': '6'...
+        }
+    """
+
     result =None
     figdata=None
     figdata2=None
+
     if (len(feature_datasetName)>0):
         if model =="som":
-            from Clust.clust.ML.clustering import som_visual
-            result, figdata, figdata2 = som_visual.somTrain(feature_dataset, feature_datasetName)
+            from Clust.clust.ML.clustering.somClustering import SomClustering
+            somV= SomClustering(feature_dataset, feature_datasetName, x, y)
+            result = somV.train()
+            figdata, figdata2= somV.make_figs()
     
     return result, figdata, figdata2
