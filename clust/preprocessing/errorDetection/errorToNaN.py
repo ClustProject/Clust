031f65c403a7e7e01c9cddd57f52441348664c07
jwmoonP <jaewonmoon.keti@gmail.com>
2022-12-22 18:52:14 +0900

    update code

--- clust/preprocessing/errorDetection/errorToNaN.py
+++ clust/preprocessing/errorDetection/errorToNaN.py
@@ -1,4 +1,40 @@
 class errorToNaN():
+    def __init__(self):
+        # Uncertain Remove 에 대한 조절 파라미터 필요 # input parameter로 받아야 함
+        # 지금은 강제 True 설정 더 정교해야 Uncertain에 대해서 잘 control 가능해 보임
+
+        # dataRangeInfoManager 대신에 limit_min_max 값을  outlier_param의 값으로 받아들이도록 수정해야 함.
+        pass
+
+    def getDataWithCertainNaN(self, data, CertainParam):
+        self.limit_min_max = CertainParam['data_min_max_limit']
+
+        if CertainParam['flag'] ==True:  
+            from Clust.clust.preprocessing.errorDetection import certainError
+            anomal_value_list=[99.9, 199.9, 299.9, 9999, -99.9, -199.9, -299.9, -9999, -9999.0] 
+            #anomal_value_list=[]
+            datawithMoreCertainNaN = certainError.CertainErrorRemove(data, self.limit_min_max, anomal_value_list).getDataWitoutcertainError()  
+        else:
+            datawithMoreCertainNaN = data.copy()
+        return datawithMoreCertainNaN
+    
+    def getDataWithUncertainNaN(self, data, uncertainParam):    
+        if uncertainParam['flag'] == True:
+            from Clust.clust.preprocessing.errorDetection import unCertainError
+            param = uncertainParam['param']
+            data_outlier = unCertainError.unCertainErrorRemove(data, param)
+            outlierIndex = data_outlier.getNoiseIndex()
+            datawithMoreUnCertainNaN = data_outlier.getDataWithoutUncertainError(outlierIndex)
+
+        else:
+            datawithMoreUnCertainNaN = data.copy()
+        return datawithMoreUnCertainNaN
+
+
+
+
+"""
+class errorToNaN():
     def __init__(self, data_type='air'):
         # Uncertain Remove 에 대한 조절 파라미터 필요 # input parameter로 받아야 함
         # 지금은 강제 True 설정 더 정교해야 Uncertain에 대해서 잘 control 가능해 보임
@@ -34,4 +70,4 @@ class errorToNaN():
             datawithMoreUnCertainNaN = data.copy()
         return datawithMoreUnCertainNaN
 
-
+"""
