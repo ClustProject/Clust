9e6ea1d46d37226b4c480c53d229873bff5f08b1
hwangjisoo <ghkdwltn8@gmail.com>
2023-01-06 16:34:21 +0900

    heatmap update

--- clust/tool/plot/plot_eda.py
+++ clust/tool/plot/plot_eda.py
@@ -18,7 +18,7 @@ def show_one_comaparing_distributions_of_multiple_data(dataset, feature):
     """
     pass
 
-def show_one_feature_data_based_on_two_times(data, time_criteria):
+def show_one_feature_data_based_on_two_times(data, feature, time_criteria):
     """
     A function that visualizes the amount of change in a specific feature of one data as a heatmap based on two time standards.
 
@@ -42,35 +42,39 @@ def show_one_feature_data_based_on_two_times(data, time_criteria):
     # down sampling by donwsampling_freq (x_frequency)
     data = DataUpDown().data_down_sampling(data, downsampling_freq)
 
-    # split data by y_frequency
-    feature_cycle = re.findall('\D+', y_frequency)[0]
-    feature_cycle_times = int(re.findall('\d+', y_frequency)[0])
-    split_dataset = dataByCycle.getCycleselectDataFrame(data, feature_cycle, feature_cycle_times, downsampling_freq)
+    try:
+        # split data by y_frequency
+        feature_cycle = re.findall('\D+', y_frequency)[0]
+        feature_cycle_times = int(re.findall('\d+', y_frequency)[0])
+        split_dataset = dataByCycle.getCycleselectDataFrame(data[[feature]], feature_cycle, feature_cycle_times, downsampling_freq)
 
-    # get heatmap x-axis, y-axis value
-    x = list(range(1,len(split_dataset[0])+1))
-    y = list(range(1, len(split_dataset)+1))
+        # get heatmap x-axis, y-axis value
+        x = list(range(1,len(split_dataset[0])+1))
+        y = list(range(1, len(split_dataset)+1))
 
-    # get heatmap z(data) value
-    z = []
-    for split_data in split_dataset:
-        z.append(split_data.values.tolist())
-    
-    fig = go.Figure(data = go.Heatmap(z=z, x=x, y=y, hoverongaps=False))
+        # get heatmap z(data) value
+        z = []
+        for split_data in split_dataset:
+            z.append(split_data[feature].values.tolist())
+        
+        fig = go.Figure(data = go.Heatmap(z=z, x=x, y=y, hoverongaps=False))
 
-    fig.update_layout(
-        xaxis = dict(
-            tickmode = 'array',
-            tickvals = x,
-            ticktext = [str(num)+string_time for num in x]
-        ),
-        yaxis = dict(
-            tickmode = 'array',
-            tickvals = y,
-            ticktext = [str(num)+feature_cycle for num in y]
-        ))
+        fig.update_layout(
+            title = feature,
+            xaxis = dict(
+                tickmode = 'array',
+                tickvals = x,
+                ticktext = [str(num)+string_time for num in x]
+            ),
+            yaxis = dict(
+                tickmode = 'array',
+                tickvals = y,
+                ticktext = [str(num)+feature_cycle for num in y]
+            ))
 
-    fig.show()
+        fig.show()
+    except ZeroDivisionError:
+        print("The duration of the data is less than {}.".format(y_frequency))
 
 def show_one_data_distribution_of_two_feature(data, feature_list):
     """
