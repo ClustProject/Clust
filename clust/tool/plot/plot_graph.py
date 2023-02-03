faade407b8d4626a889e37908f68734853d7b47a
jwmoonP <jaewonmoon.keti@gmail.com>
2023-02-03 12:06:40 +0900

    plot correlation

--- clust/tool/plot/plot_graph.py
+++ clust/tool/plot/plot_graph.py
@@ -1,19 +1,25 @@
 import matplotlib.pyplot as plt
 
-def plot_correlation_chart(data):
+# def plot_correlation_chart(data):
+
+def plot_heatmap(data):
     """
-    Plot Correlation Chart
+    plot heatmap plt
 
     Args:
         data (dataFrame): input data 
+
+    Returns:
+        
     """
     import seaborn as sns
-    corr = data.corr()
-    ax = sns.heatmap(corr, xticklabels = corr.columns.values, yticklabels = corr.columns.values, annot =True, annot_kws ={'size': 8})
+    
+    ax = sns.heatmap(data, xticklabels = data.columns.values, yticklabels = data.columns.values, annot =True, annot_kws ={'size': 8})
     bottom, top = ax.get_ylim() 
     heat_map = plt.gcf()
     ax.set_ylim(bottom+0.5, top-0.5)
     heat_map.set_size_inches(10, 6)
-    
+
     return plt
+
     
