91508cc41fd521f6ef9c6afda87bf70b5dc6c664
jwmoonP <jaewonmoon.keti@gmail.com>
2023-02-16 10:01:18 +0900

    update data code

--- clust/ingestion/influx/ms_data.py
+++ clust/ingestion/influx/ms_data.py
@@ -1,5 +1,62 @@
 import pandas as pd
 
+def get_general_process_param():
+    refine_param        = {"removeDuplication":{"flag":True},"staticFrequency":{"flag":True, "frequency":None}}
+    CertainParam        = {'flag': True}
+    uncertainParam      = {'flag': False, "param": {"outlierDetectorConfig":[{'algorithm': 'IQR', 'percentile':99 ,'alg_parameter': {'weight':100}}]}}
+    outlier_param       = {"certainErrorToNaN":CertainParam, "unCertainErrorToNaN":uncertainParam}
+    imputation_param    = {
+        "flag":True,
+        "imputation_method":[{"min":0,"max":3,"method":"linear", "parameter":{}}],
+        "totalNonNanRatio":80
+    }
+
+    # 최종 파라미터
+    process_param       = {'refine_param':refine_param, 'outlier_param':outlier_param, 'imputation_param':imputation_param}
+    return process_param
+
+def get_integration_param(integration_freq_sec):
+    integration_param   = {
+        "integration_duration":"common",
+        "integration_frequency":integration_freq_sec,
+        "param":{},
+        "method":"meta"
+    }
+    return integration_param
+    
+def get_integated_multi_ms(data_param, db_client):
+    """get integrated numeric data with multiple MS data
+
+    Args:
+        data_param (_type_): data_param 
+        db_client (_type_): influx db client
+
+    Returns:
+        _type_: _description_
+    """
+    
+    ms_array        = data_param['ms_array']
+    start_time          = data_param['start_time']
+    end_time            = data_param['end_time']
+    integration_freq_sec = int(data_param['integration_freq_min']) * 60 
+    
+    #TODO 추후 이부분 외부 입력 받도록
+    integration_param = get_integration_param(integration_freq_sec)
+    process_param = get_general_process_param()
+    
+
+    from Clust.clust.integration.utils import param
+    intDataInfo = param.makeIntDataInfoSet(ms_array, start_time, end_time) 
+    from Clust.clust.ingestion.influx import ms_data
+    multiple_dataset  = get_only_numericData_in_ms(db_client, intDataInfo)  
+    
+    # data Integration
+    from Clust.clust.integration.integrationInterface import IntegrationInterface
+    dataIntegrated = IntegrationInterface().multipleDatasetsIntegration(process_param, integration_param, multiple_dataset)
+    
+    return dataIntegrated
+
+
 def get_only_numericData_in_ms(db_client, intDataInfo):
     """
     Get measurement Data Set according to the dbinfo
