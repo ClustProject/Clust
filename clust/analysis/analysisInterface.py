82344e2c8b8b77c1112ff5f1710190e3404a7538
jwmoonP <jaewonmoon.keti@gmail.com>
2023-02-20 20:57:26 +0900

    update claSS

--- clust/analysis/analysisInterface.py
+++ clust/analysis/analysisInterface.py
@@ -49,7 +49,7 @@ def get_analysis_by_data(analysis_method, analysis_param, input_df):
     elif analysis_method == 'max_correlation_value_index_with_lag':
         df_analysis = da.get_max_correlation_table_with_lag(analysis_param, input_df) 
         
-    return df_analysis
+    return df_analysisget_multiple_max_correlation_index_table_with_lag
 
 def get_analysis_by_data_set(analysis_method, analysis_param, input_df_set):
     """ input이 dataframe일 경우 분석을 수행하는 함수
