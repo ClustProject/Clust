51db74884346c709ca1adef2437d278930ea77a2
keymemory <keymemory@naver.com>
2023-03-09 15:40:47 +0900

    classification update

--- clust/ML/classification/test.py
+++ clust/ML/classification/test.py
@@ -5,19 +5,22 @@ import numpy as np
 sys.path.append("..")
 sys.path.append("../..")
 
-from torch.utils.data import TensorDataset, DataLoader
-from Clust.clust.ML.common.inference import Inference
 from Clust.clust.transformation.type.DFToNPArray import trans_df_to_np
 
+from Clust.clust.ML.classification.classification_model.cnn_1d_model import CNNModel
+from Clust.clust.ML.classification.classification_model.fc_model import FCModel
+from Clust.clust.ML.classification.classification_model.lstm_fcns_model import LSTMFCNsModel
+from Clust.clust.ML.classification.classification_model.rnn_model import RNNModel
 
-class ClassificationTest(Inference):
+
+class ClassificationTest():
     def __init__(self):
         """
         """
         super().__init__()
         
 
-    def set_param(self, param):
+    def set_param(self, params):
         """
         Set Parameter for Test
 
@@ -37,19 +40,48 @@ class ClassificationTest(Inference):
             ...            "n_epochs":10    }
 
         """
-        self.batch_size = param['batch_size']
-        self.device = param['device']
+        self.params = params
+        self.batch_size = params['batch_size']
+        self.device = params['device']
 
 
-    def set_data(self, X, y, windowNum= 0, dim=None):
+    def set_model(self, model_method, model_file_path):
         """
-        set data for test & transform data
+        Build model and return initialized model for selected model_name
+
+        Args:
+            model_method (string): model method name
+        """
+        model_method = model_method
+        if model_method == 'LSTM_cf':
+            self.params["rnn_type"] = 'lstm'
+        elif self.model_method == 'GRU_cf':
+            self.params["rnn_type"] = 'gru'
+        
+        # build initialized model
+        if (model_method == 'LSTM_cf') | (model_method == "GRU_cf"):
+            self.model = RNNModel(**self.params)
+        elif model_method == 'CNN_1D_cf':
+            self.model = CNNModel(**self.params)
+        elif model_method == 'LSTM_FCNs_cf':
+            self.model = LSTMFCNsModel(**self.params)
+        elif model_method == 'FC_cf':
+            self.model = FCModel(**self.params)
+        else:
+            print('Choose the model correctly')
+
+        self.model.load_model(model_file_path)
+
+
+    def set_data(self, test_X, test_y, window_num=0, dim=None):
+        """
+        set data for test
 
         Args:
             test_X (dataframe): Test X data
             test_y (dataframe): Test y data
             window_num (integer) : window size
-            dim (integer) : dimension
+
 
         Example:
 
@@ -57,110 +89,24 @@ class ClassificationTest(Inference):
             ...         test_X : test X data
             ...         test_y : test y data
             ...         window_num : window size
-            ...         dim : dimension
 
+        """  
+        self.test_loader = self.model.create_testloader(self.batch_size, test_X, test_y, window_num, dim)
+
+
+    def test(self):
         """
-        self.test_X, self.test_y = trans_df_to_np(X, y, windowNum, dim)
-
-
-    
-    def get_result(self, model):
-        """
-        Predict RegresiionResult based on model result
-
-        Args:
-            model (model) : load train model
+        Test model and return result
 
         Returns:
-            preds (ndarray) : prediction data
-            probs (ndarray) : prediction probabilities
-            trues (ndarray) : original data
-            acc (float) : test accuracy
-        
+            preds (ndarray): prediction data
+            trues (ndarray): original data
+            mse (float): mean square error  # TBD
+            mae (float): mean absolute error    # TBD
         """
         print("\nStart testing data\n")
-        test_loader = self._get_test_loader()
-        
-        # load best model
-        
-        # get prediction and accuracy
-        preds, probs, trues, acc = self._test(model, test_loader)
-        print(f'** Performance of test dataset ==> PROB = {probs}, ACC = {acc}')
-        print(f'** Dimension of result for test dataset = {preds.shape}')
+        preds, probs, trues, acc = self.model.test(self.params, self.test_loader, self.device)
 
         return preds, probs, trues, acc
 
-
-
-    def _get_test_loader(self):
-        """
-        get TestLoader
-
-        Returns:
-            test_loader (DataLoader) : data loader
-        """
-
-        x_data = np.array(self.test_X)
-        y_data = self.test_y
-        test_data = TensorDataset(torch.Tensor(x_data), torch.Tensor(y_data))
-        test_loader = DataLoader(test_data, batch_size=self.batch_size, shuffle=True)
-
-        return test_loader
-
-
-
-
-    def _test(self, model, test_loader):
-        """
-        Predict classes for test dataset based on the trained model
-
-        Args:
-            model (model) : load train model
-            test_loader (DataLoader) : data loader
-
-        Returns:
-            preds (ndarray) : prediction data
-            probs (ndarray) : prediction probabilities
-            trues (ndarray) : original data
-            acc (float) : test accuracy
-        """
-        model.eval()   # 모델을 validation mode로 설정
-        
-        # test_loader에 대하여 검증 진행 (gradient update 방지)
-        with torch.no_grad():
-            corrects = 0
-            total = 0
-            preds = []
-            probs = []
-            trues = []
-            for inputs, labels in test_loader:
-                inputs = inputs.to(self.device)
-                labels = labels.to(self.device, dtype=torch.long)
-
-                model.to(self.device)
-    
-                # forwinputs = inputs.to(device)ard
-                # input을 model에 넣어 output을 도출
-                outputs = model(inputs)
-                prob = outputs
-                prob = nn.Softmax(dim=1)(prob)
-
-                # output 중 최댓값의 위치에 해당하는 class로 예측을 수행
-                _, pred = torch.max(outputs, 1)
-                
-                # batch별 정답 개수를 축적함
-                corrects += torch.sum(pred == labels.data)
-                total += labels.size(0)
-
-                preds.extend(pred.detach().cpu().numpy()) 
-                probs.extend(prob.detach().cpu().numpy())
-                trues.extend(labels.detach().cpu().numpy())
-
-            preds = np.array(preds)
-            probs = np.array(probs)
-            trues = np.array(trues)
-            
-            acc = (corrects.double() / total).item()
-        
-        return preds, probs, trues, acc
         
