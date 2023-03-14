ff939bf61cfb454ea662652958c9fcd6536ea831
jianso <dothewillof@naver.com>
2023-03-14 10:29:22 +0900

    update plot

--- clust/analysis/analysis_interface.py
+++ clust/analysis/analysis_interface.py
@@ -34,8 +34,10 @@ def get_analysis_result(analysis_method, analysis_param, input):
     
     if analysis_method in analysis_by_data_list:
         df_analysis = get_analysis_by_data(analysis_method, analysis_param, input)
+
     elif analysis_method in analysis_by_data_set_list:
         df_analysis = get_analysis_by_data_set(analysis_method, analysis_param, input)
+
         
     return df_analysis
 
@@ -75,8 +77,14 @@ def get_analysis_by_data_set(analysis_method, analysis_param, input_df_set):
      'multiple_maxabs_correlation_value_table_with_lag', 'multiple_maxabs_correlation_index_table_with_lag' 둘 중 하나의 경우 분석 수행
 
     # Args
-     * analysis_method (_str_) : analysis method
+     * analysis_method (_str_) : 분석 방법
+     ```example
+        ['multiple_maxabs_correlation_value_table_with_lag', 'multiple_maxabs_correlation_index_table_with_lag']
+     ```
      * analysis_param (_dict_) : analysis method에 따른 적절한 파라미터
+     ```example
+        'analysis_param': {'feature_key': 'PM10', 'lag_number': '24'}
+     ```
      * input_df (_pd.dataFrame_) : 분석에 필요한 인풋 데이터
 
     # Returns
@@ -87,12 +95,12 @@ def get_analysis_by_data_set(analysis_method, analysis_param, input_df_set):
     
     dsa = dataSetAnalysis.DataSetAnalysis()
     if analysis_method == 'multiple_maxabs_correlation_value_table_with_lag':
-        df_analysis = dsa.get_multiple_max_correlation_value_table_with_lag(analysis_param, input_df_set)
-        # 출력은 절대값으로
-        df_analysis = df_analysis.apply(pd.to_numeric)
+        df_analysis = dsa.get_multiple_max_correlation_value_table_with_lag(analysis_param, input_df_set)       
         
     elif analysis_method == 'multiple_maxabs_correlation_index_table_with_lag':
         df_analysis = dsa.get_multiple_max_correlation_index_table_with_lag(analysis_param, input_df_set)
-        df_analysis = df_analysis.apply(pd.to_numeric).abs()
+        
+    
+    df_analysis = df_analysis.apply(pd.to_numeric).abs()
     
     return df_analysis
