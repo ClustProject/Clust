9621b0e5685fd669bbee74d97c92f1b70fd7402f
jwmoonP <jaewonmoon.keti@gmail.com>
2023-11-16 18:41:31 +0900

    update

--- clust/tool/plot/plot_interface.py
+++ clust/tool/plot/plot_interface.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append("../")
 sys.path.append("../../")
+import numpy as np
 
 from Clust.clust.tool.plot import plot_echart, plot_image, plot_plt
 
@@ -22,7 +23,11 @@ def get_graph_result(graph_format, graph_type, df, param = None):
 
             
     """
-        
+    # by graph_type
+    if graph_type =='area':
+        df = abs(df)
+
+    # by graph_format
     if graph_format =='web':
         result = plot_echart.get_echart_json_result(graph_type, df) # return echart style json
     elif graph_format =='plt':
@@ -32,4 +37,27 @@ def get_graph_result(graph_format, graph_type, df, param = None):
         
     return result
 
+#plot_analised_graph
+
+from Clust.clust.analysis import analysis_interface 
+def scale_xy_frequency(data, analysis_method, analysis_param, plot_format, plot_type_list):
+    """       
+    한개의 데이터를 분석후 여러 plot type으로 그림을 그리는 plot 함수
+
+    Args:
+        data(_pandas.dataFrame_) :  input data
+        analysis_method(str) : one of analysis_methods
+        analysis_param(json) : parameter
+        plot_format(str): plot_format
+        plot_type_list (array) : multiple (or one) plot type list
+
+    Returns:
+        img or plt or json: result
+
+            
+    """
+    analysis_data = np.round(analysis_interface.get_analysis_result(analysis_method, analysis_param, data), 2)
+    for plot_type in plot_type_list:
+        result = get_graph_result(plot_format, plot_type, analysis_data)
+    return result
 
